199a7959d00974ca0ff6dda5335be3324bdbdf68
mohammad <normalguyinvancouver@gmail.com>
2016-10-22 15:22:59 +0000

    Main functionality built. Tests passing.

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,12 @@
 
 
 def create_box(height, width, character):
-    pass
+    for i in range(height): #loop from 0 to height 
+        print (character * width ) #prints character "width" many times
+       
+        
+        
 
 
 if __name__ == '__main__':
-    create_box(3, 4, '*')
+    create_box(3, 3, '$')
