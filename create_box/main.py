045912d3693442c14710e7026b8dc7c0e5ed32a9
mohammad <normalguyinvancouver@gmail.com>
2016-10-22 16:03:49 +0000

    this time the tests actually passed!

--- create_box/main.py
+++ create_box/main.py
@@ -2,12 +2,14 @@
 
 
 def create_box(height, width, character):
+    box = ''
     for i in range(height): #loop from 0 to height 
+        box += (character * width) + '\n'
         print (character * width ) #prints character "width" many times
-       
+    return box
         
         
 
 
 if __name__ == '__main__':
-    create_box(3, 3, '$')
+    create_box(3, 4, '*')
