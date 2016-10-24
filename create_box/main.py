b17504e9699f80d9e6a2d1b31a7bad5d88c84155
mohammad <normalguyinvancouver@gmail.com>
2016-10-24 19:17:02 +0000

    Main functionality built, and extra challenge test passing

--- create_box/main.py
+++ create_box/main.py
@@ -1,15 +1,28 @@
-"""This is the entry point of the program."""
-
-
 def create_box(height, width, character):
+    
     box = ''
-    for i in range(height): #loop from 0 to height 
+    
+    for i in range(height):
         box += (character * width) + '\n'
-        print (character * width ) #prints character "width" many times
+  
     return box
-        
-        
 
-
+#extra challenge    
+def create_empty_box(height, width, character):
+    
+    box = ''
+    hollow_string_space = " " # one space unit
+    
+    for i in range(height):
+        if (i == 0 or i == height-1):
+            box += (character * width)
+        else:
+            box += (character + hollow_string * (width-2) + character)
+    
+    return box
+    
 if __name__ == '__main__':
-    create_box(3, 4, '*')
+    create_box(5, 10, '*')
+    create_empty_box(5,5,'x')            
+            
+
